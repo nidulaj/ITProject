f1f996d279c2a218993235285bf9ee7178eb8e6c
nidulaj <nidulajayalath@gmail.com>
2025-08-18 12:08:25 +0530

    db connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const {connectDB} = require('./db/dbConnect');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+connectDB();
 
 app.listen(5000, () => {
   console.log('Server started on http://localhost:5000');
