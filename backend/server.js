618040698f5f53a8185d14d586c7efc19c49aafe
nidulaj <nidulajayalath@gmail.com>
2025-08-20 09:42:03 +0530

    Feature/order management: order, product crud (#8)
    * db connection (#2)
    
    * setup frontend
    
    * db connection
    
    * category is null,others working
    
    * category null issue solved
    
    * create and read working
    
    * create,read,update done
    
    * product CRUD done
    
    * order create,view orders,update payment status,update order status done
    
    ---------
    
    Co-authored-by: IndujaThathsarani <it23818316@my.sliit.lk>

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const {connectDB} = require('./db/dbConnect');
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+
 
 const app = express();
 app.use(cors());
@@ -9,6 +12,10 @@ app.use(express.json());
 
 connectDB();
 
+app.use('/api/products', productRoutes);
+app.use('/api/orders',orderRoutes);
+
 app.listen(5000, () => {
   console.log('Server started on http://localhost:5000');
 });
+
