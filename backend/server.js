09af4d1f7ea0ac18c6ad5312cafb7d9bfba66b0b
nidulaj <nidulajayalath@gmail.com>
2025-08-20 16:03:05 +0530

    Development (#18)
    * Feature/user management to development (#13)
    
    * db connection (#2)
    
    * setup frontend
    
    * db connection
    
    * customer registration
    
    * login
    
    * login and user profile
    
    * remove delete profile
    
    * self delete profile by customer (#4)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies (#5)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies
    
    * send sms (#6)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies
    
    * send sms
    
    * send verification code error fix (#7)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies
    
    * send sms
    
    * send verification code error fix
    
    * email code verifiy (#9)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies
    
    * send sms
    
    * send verification code error fix
    
    * email code verification
    
    * Feature/user management customer management (#12)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies
    
    * send sms
    
    * send verification code error fix
    
    * email code verification
    
    * setup folders in frontend
    
    * user management clean
    
    * Feature/order management to development (#14)
    
    * db connection (#2)
    
    * setup frontend
    
    * db connection
    
    * category is null,others working
    
    * category null issue solved
    
    * create and read working
    
    * create,read,update done
    
    * product CRUD done
    
    * order create,view orders,update payment status,update order status done
    
    ---------
    
    Co-authored-by: IndujaThathsarani <it23818316@my.sliit.lk>
    
    * Feature/production management to development : recipe crud (#15)
    
    * db connection (#2)
    
    * setup frontend
    
    * db connection
    
    * recipe crud
    
    ---------
    
    Co-authored-by: SadisaRashmika <sadisa.rashmika@gmail.com>
    
    * Feature/inventory management to development : crud done (#16)
    
    * db connection (#2)
    
    * setup frontend
    
    * db connection
    
    * CRUD of ingredient done.
    
    * CRUD of special ingredient done
    
    * CRUD of Final Products done
    
    * CRUD of storagezones done
    
    ---------
    
    Co-authored-by: Imasha-N <imaaathapattu@gmail.com>
    
    * Feature/financial management to development: discount logic (#17)
    
    * db connection (#2)
    
    * setup frontend
    
    * db connection
    
    * discount crud
    
    * discount logic
    
    ---------
    
    Co-authored-by: Minu004 <minulijayasinghe04@gmail.com>
    
    ---------
    
    Co-authored-by: IndujaThathsarani <it23818316@my.sliit.lk>
    Co-authored-by: SadisaRashmika <sadisa.rashmika@gmail.com>
    Co-authored-by: Imasha-N <imaaathapattu@gmail.com>
    Co-authored-by: Minu004 <minulijayasinghe04@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,20 @@ require('dotenv').config();
 const cors = require('cors');
 const {connectDB} = require('./db/dbConnect');
 
-
+const discountRoutes = require('./routes/discountRoutes')
+const ingredientRoutes = require('./routes/ingredientRoutes');
+const specialRoutes=require('./routes/specialRoutes');
+const finalRoutes=require('./routes/finalRoutes');
+const storeRoutes=require('./routes/storeRoutes');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const customerAuthRoute = require('./routes/customerAuthRoute')
+const recipeRoutes = require("./routes/recipeRoutes");
+
 
 
 const app = express();
 const cookieParser = require('cookie-parser')
-
-
 
 app.use(cors());
 app.use(express.json());
@@ -21,14 +25,15 @@ app.use(cookieParser());
 connectDB();
 
 app.use('/auth/customer', customerAuthRoute);
-
 app.use('/api/products', productRoutes);
 app.use('/api/orders',orderRoutes);
-
-
-
-
+app.use("/api/recipe", recipeRoutes);
+app.use('/api/ingredient', ingredientRoutes);
+app.use('/api/special',specialRoutes);
+app.use('/api/final',finalRoutes);
+app.use('/api/store',storeRoutes);
+app.use('/api/discounts', discountRoutes);
 
 app.listen(5000, () => {
-  console.log('Server started on http://localhost:5000');
+  console.log("Server started on http://localhost:5000");
 });
