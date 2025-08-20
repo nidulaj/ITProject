49fde7fe95750b84743d4e461f0a41a78592d22e
nidulaj <nidulajayalath@gmail.com>
2025-08-20 15:14:57 +0530

    Feature/user management to development (#13)
    * db connection (#2)
    
    * setup frontend
    
    * db connection
    
    * customer registration
    
    * login
    
    * login and user profile
    
    * remove delete profile
    
    * self delete profile by customer (#4)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies (#5)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies
    
    * send sms (#6)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies
    
    * send sms
    
    * send verification code error fix (#7)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies
    
    * send sms
    
    * send verification code error fix
    
    * email code verifiy (#9)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies
    
    * send sms
    
    * send verification code error fix
    
    * email code verification
    
    * Feature/user management customer management (#12)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies
    
    * send sms
    
    * send verification code error fix
    
    * email code verification
    
    * setup folders in frontend
    
    * user management clean

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const cors = require('cors');
 const {connectDB} = require('./db/dbConnect');
 
+
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const customerAuthRoute = require('./routes/customerAuthRoute')
@@ -12,6 +13,7 @@ const app = express();
 const cookieParser = require('cookie-parser')
 
 
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
@@ -19,8 +21,10 @@ app.use(cookieParser());
 connectDB();
 
 app.use('/auth/customer', customerAuthRoute);
+
 app.use('/api/products', productRoutes);
 app.use('/api/orders',orderRoutes);
+
 
 
 
@@ -28,4 +32,3 @@ app.use('/api/orders',orderRoutes);
 app.listen(5000, () => {
   console.log('Server started on http://localhost:5000');
 });
-
