5198c365b32b7d70d0cfc916277af6c0a7ae7982
nidulaj <nidulajayalath@gmail.com>
2025-08-20 10:58:52 +0530

    Feature/user management: customer management without forgot password and email verify (#11)
    * db connection (#2)
    
    * setup frontend
    
    * db connection
    
    * customer registration
    
    * login
    
    * login and user profile
    
    * remove delete profile
    
    * self delete profile by customer (#4)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies (#5)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies
    
    * send sms (#6)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies
    
    * send sms
    
    * send verification code error fix (#7)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies
    
    * send sms
    
    * send verification code error fix
    
    * email code verifiy (#9)
    
    * remove delete profile
    
    * self delete profile by customer
    
    * accessToken, refreshToken and cookies
    
    * send sms
    
    * send verification code error fix
    
    * email code verification

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,28 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const {connectDB} = require('./db/dbConnect');
+
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const customerAuthRoute = require('./routes/customerAuthRoute')
 
 
 const app = express();
+const cookieParser = require('cookie-parser')
+
+
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 connectDB();
 
+app.use('/auth/customer', customerAuthRoute);
 app.use('/api/products', productRoutes);
 app.use('/api/orders',orderRoutes);
+
+
+
 
 app.listen(5000, () => {
   console.log('Server started on http://localhost:5000');
