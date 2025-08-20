2d0afb2a414eebf60d0730f5e6c6d209ab1a4093
Imasha-N <imaaathapattu@gmail.com>
2025-08-20 09:39:12 +0530

    CRUD of ingredient done.

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,17 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const {connectDB} = require('./db/dbConnect');
+const ingredientRoutes = require('./routes/ingredientRoutes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 connectDB();
-
+app.use('/api/ingredient', ingredientRoutes);
 app.listen(5000, () => {
   console.log('Server started on http://localhost:5000');
 });
+
+
+ 
