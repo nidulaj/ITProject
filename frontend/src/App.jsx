1c7a4cac9300f962b74ac7f94db6448e3ccc94d2
IndujaThathsarani <it23818316@my.sliit.lk>
2025-08-28 02:24:31 +0530

    order managemnt folders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ProductCatalog from './features/order-management/pages/ProductCatalog'
+import './App.css'
 
 function App() {
-
   return (
     <>
-      
+      <ProductCatalog />
     </>
   )
 }
--- frontend/src/features/order-management/components/index.js
+++ frontend/src/features/order-management/components/index.js
@@ -1,4 +1,3 @@
-export { default as ProductCatalog } from './ProductCatalog';
 export { default as ProductForm } from './ProductForm';
 export { default as ProductCard } from './ProductCard';
 export { default as ProductGrid } from './ProductGrid';
