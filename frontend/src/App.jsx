43c510625af7ed386a3c76f5e76b568a39da9f3a
Imasha-N <imaaathapattu@gmail.com>
2025-09-23 11:22:42 +0530

    crud done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Dashboard from "./features/inventory-management/pages/Dashboard";
 
 function App() {
 
   return (
-    <>
+    <div>
+     <Dashboard />;
       
-    </>
+    </div>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import "./index.css";  // <-- Make sure this is correct
+ 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+ 
+ 
