815940e5281f0a19672575ee128eb3129ea2634b
Minu004 <minulijayasinghe04@gmail.com>
2025-09-25 09:58:19 +0530

    refreshing dashboard

--- frontend/src/features/financial-management/pages/PaymentFormPage.jsx
+++ frontend/src/features/financial-management/pages/PaymentFormPage.jsx
@@ -1,18 +1,3 @@
-/*import React from "react";
-import PaymentForm from "../components/PaymentForm";
-
-const PaymentFormPage = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-6">
-      <PaymentForm />
-    </div>
-  );
-};
-
-export default PaymentFormPage;*/
-
-
-
 import React from "react";
 import PaymentForm from "../components/PaymentForm";
 
