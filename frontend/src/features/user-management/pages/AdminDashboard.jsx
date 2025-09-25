f00d1cc546e0dbd23219d411955b4a0a0b80fd91
nidulaj <nidulajayalath@gmail.com>
2025-09-25 10:01:37 +0530

    first name displaying admin dashboard

--- frontend/src/features/user-management/pages/AdminDashboard.jsx
+++ frontend/src/features/user-management/pages/AdminDashboard.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Sidebar from "../components/Slidebar";
 import Header from "../components/Header";
 import StaffManagement from "../components/StaffManagement";
@@ -10,14 +11,35 @@ import ProfileSecurity from "../components/ProfileSecurity";
 import RoleInfo from "../components/RoleInfo";
 import StaffUserInfo from "../components/staffUserInfo";
 import CustomerManagement from "../components/customerManagement";
+import UserProfile from "../components/UserProfile";
+import { authFetch } from "../utils/authFetchStaff";
 
 export default function AdminDashboard() {
+  const [userInfo, setUserInfo] = useState(null);
+
+
+  useEffect(() => {
+      const fetchUserInfo = async () => {
+      try {
+          const res = await authFetch({
+            method: "get",
+            url: `http://localhost:5000/api/staff/auth/userInfo`,
+          });
+          setUserInfo(res.data);
+    } catch (error) {
+      console.error("Error fetching user info:", error);
+    }
+  };
+  fetchUserInfo();
+ 
+  }, []);
+
   return (
     <div className="min-h-screen flex bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
       <Sidebar />
 
       <main className="flex-1 p-6 space-y-8 overflow-y-auto ml-64">
-        <Header />
+        <Header userInfo={userInfo} />
         <Routes>
           <Route path="/" element={<h2 className="text-lg font-bold">Welcome to Admin Dashboard</h2>} />
           <Route path="staff" element={<StaffManagement />} />
@@ -29,6 +51,7 @@ export default function AdminDashboard() {
           <Route path="complaints" element={<Complaints />} />
           <Route path="chat" element={<Chat />} />
           <Route path="profileSecurity" element={<ProfileSecurity />} />
+          <Route path="userProfile" element={<UserProfile />} />
         </Routes>
       </main>
     </div>
